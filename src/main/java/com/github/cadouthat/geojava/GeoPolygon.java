300f42e85071e00abeea41b8d66d9812e8e6e396
Connor Douthat <connor@steelcastle.biz>
2017-08-10 16:36:28 -0700

    Avoid double-counting intersections near polygon vertices

--- src/main/java/com/github/cadouthat/geojava/GeoPolygon.java
+++ src/main/java/com/github/cadouthat/geojava/GeoPolygon.java
@@ -10,6 +10,11 @@ import java.util.List;
 public class GeoPolygon {
 
     /**
+     * The minimum distance between intersection points for them to be considered unique, in metres
+     */
+    static final double UNIQUE_INTERSECTION_DIST = 0.1;
+
+    /**
      * Any point known to be outside the polygon, used as the destination for even-odd intersection tests,
      * defaults to the North Pole
      */
@@ -43,19 +48,38 @@ public class GeoPolygon {
         // The arc between the point and external reference will be used for testing
         GeoArc pointArc = new GeoArc(point, externalReference);
 
-        // Count the number of intersections with edges
-        int intersections = 0;
-
+        GeoPoint[] intersectionPoints = new GeoPoint[vertices.size()];
         for (int i = 0; i < vertices.size(); i++) {
             // Check each arc between adjacent vertices
             GeoPoint vertex1 = vertices.get(i);
-            GeoPoint vertex2 = (i > 0) ? vertices.get(i - 1) : vertices.get(vertices.size() - 1);
+            GeoPoint vertex2 = (i > 0) ?
+                    vertices.get(i - 1) :
+                    vertices.get(vertices.size() - 1);
             GeoArc vertexArc = new GeoArc(vertex1, vertex2);
 
-            // Location of intersection doesn't matter, only the presence of intersection
-            if (pointArc.intersect(vertexArc) != null) {
-                intersections++;
+            // Store the intersection point on this edge (if any)
+            intersectionPoints[i] = pointArc.intersect(vertexArc);
+        }
+
+        // Count the number of unique intersections
+        int intersections = 0;
+        for (int i = 0; i < intersectionPoints.length; i++) {
+            GeoPoint iPoint = intersectionPoints[i];
+            if (iPoint == null) continue;
+
+            // If there was an intersection on the previous edge, determine if iPoint is a duplicate
+            GeoPoint prevPoint = (i > 0) ?
+                    intersectionPoints[i - 1] :
+                    intersectionPoints[intersectionPoints.length - 1];
+            if (prevPoint != null) {
+                GeoArc comparisonArc = new GeoArc(prevPoint, iPoint);
+                if (comparisonArc.length() <= UNIQUE_INTERSECTION_DIST) {
+                    // iPoint will not be counted, but prevPoint has or will be
+                    iPoint = null;
+                }
             }
+
+            if (iPoint != null) intersections++;
         }
 
         // The point is considered inside the polygon if the number of intersections is odd
